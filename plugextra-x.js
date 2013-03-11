84db3fdb2dd0332c2e88f115236fb3af6a996e5f
TeeheeLP <teeheelp@gmx-topmail.de>
2013-03-11 15:35:49 +0100

    Update plugextra-x.js

--- plugextra-x.js
+++ plugextra-x.js
@@ -244,15 +244,16 @@ function hideUserList()
 }
 
 var hidelistbut = document.createElement("div");
-hidelistbut.style.backgroundColor = "#666666";
+hidelistbut.style.backgroundColor = "#333333";
 hidelistbut.style.borderRadius = "7px";
-hidelistbut.style.boxShadow = "0px 0px 4px #000000, -1px -1px 15px #333333";
+hidelistbut.style.boxShadow = "0px 0px 4px #000000, -1px -1px 15px #111111 inset";
 hidelistbut.style.width = "100%";
 hidelistbut.style.textAlign = "center";
 hidelistbut.style.height = "2.5em";
 hidelistbut.style.display = "block";
 hidelistbut.style.cursor = "pointer";
 hidelistbut.style.lineHeight = "2.5em";
+hidelistbut.style.marginBottom = "5px";
 hidelistbut.innerHTML = "Hide";
 hidelistbut.onclick = function() { hideUserList(); };
 
