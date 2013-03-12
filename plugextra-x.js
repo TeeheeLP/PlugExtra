d468c43da927199595efda1f4950d243cc79590b
TeeheeLP <teeheelp@gmx-topmail.de>
2013-03-12 18:44:12 +0100

    Update plugextra-x.js

--- plugextra-x.js
+++ plugextra-x.js
@@ -276,6 +276,7 @@ awaymsgin.style.borderRadius = "5px";
 awaymsgin.style.boxShadow = "1px 1px 3px #000000 inset";
 awaymsgin.style.border = "2px solid #FFFFFF";
 awaymsgin.style.backgroundColor = "#FFFFFF";
+awaymsgin.value = "I'm away";
 
 userlist.appendChild(awaymsgin);
 
