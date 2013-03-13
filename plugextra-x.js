5662f2cdc80492cb469c463b543998bc158a5f6e
TeeheeLP <teeheelp@gmx-topmail.de>
2013-03-13 16:47:36 +0100

    Update plugextra-x.js

--- plugextra-x.js
+++ plugextra-x.js
@@ -627,82 +627,84 @@ function checkOwnIn(e, chatin)
 	var iscommand = true;
 	var commandinfo = chatin.value.split(' ');
 	
-	printChat(e.keyCode);
-	switch(commandinfo[0])
+	if (e.keyCode == 13)
 	{
-		case "$help":
-			printChat("Here is a list of available commands:<br> \
-				( <> = optional [] = necessary )<br> \
-				$manual: Shows how to use the plugin<br> \
-				$help: Displays this message<br> \
-				$version: Displays the current version<br> \
-				$changes: Shows the newest changes<br> \
-				$reset: Resets the log position<br> \
-				$away <message>: Activates or deactivates the awaybot");
-			break;
-		case "$version":
-			printChat("Running on version " + version);
-			break;
-		case "$manual":
-			printChat("What information do you need?<br> \
-				$list: Information about the userlist<br> \
-				$log: Information about the log<br> \
-				$buttons: Information about the log buttons<br> \
-				$awaybot: Information about the awaybot");
-			break;
-		case "$list":
-			printChat("The userlist is at the left hand side of the screen. You can click at the very left \
-				to show it if it's hidden. It contains a list of all users currently in the waitlist \
-				and in the room.");
-			break;
-		case "$log":
-			printChat("The log displays information suchs as woots and mehs for the current song. \
-				You can move the log in the middle of the screen by dragging it \
-				with the top bar. Double click it or enter $reset to move the log back to \
-				it's original position. Use the black button at its bottom right hand corner to hide \
-				or show the log.");
-			break;
-		case "$button":
-			printChat("When a log button is pressed it begins to glow slightly. The blue button \
-				toggles autojoin which makes you automatically join the waitlist if you're not \
-				in it already, while the green button toggles autowoot which makes you woot every song.");
-			break;
-		case "$awaybot":
-			printChat("By clicking the 'Away'-button in the userlist or using the $away command you \
-				automatically reply with a specified message whenever somebody is mentioning you.");
-			break;
-		case "$changes":
-			printChat("Recent changes: Added commands and the waitlist.");
-			break;
-		case "$reset":
-			var log = document.getElementById("log");
-			log.style.top = "288px";
-			log.style.right = "177px";
-			log.style.zIndex = "8";
-			var but1 = document.getElementById("togg");
-			but1.style.top = "280px";
-			but1.style.left = "225px";
-			var but2 = document.getElementById("expwoot");
-			but2.style.top = "255px";
-			but2.style.left = "198px";
-			var but3 = document.getElementById("expjoin");
-			but3.style.top = "230px";
-			but3.style.left = "171px";
-			printChat("Reset the log position.");
-			break;
-		case "$away":
-			if (commandinfo.length > 1)
-				printChat("Working!");
-			document.getElementById("awaybutx").click();
-			break;
-		default:
-			iscommand = false;
-			break;
-	}
-	
-	if (iscommand)
-	{
-		chatin.value = "";
+		switch(commandinfo[0])
+		{
+			case "$help":
+				printChat("Here is a list of available commands:<br> \
+					( <> = optional [] = necessary )<br> \
+					$manual: Shows how to use the plugin<br> \
+					$help: Displays this message<br> \
+					$version: Displays the current version<br> \
+					$changes: Shows the newest changes<br> \
+					$reset: Resets the log position<br> \
+					$away <message>: Activates or deactivates the awaybot");
+				break;
+			case "$version":
+				printChat("Running on version " + version);
+				break;
+			case "$manual":
+				printChat("What information do you need?<br> \
+					$list: Information about the userlist<br> \
+					$log: Information about the log<br> \
+					$buttons: Information about the log buttons<br> \
+					$awaybot: Information about the awaybot");
+				break;
+			case "$list":
+				printChat("The userlist is at the left hand side of the screen. You can click at the very left \
+					to show it if it's hidden. It contains a list of all users currently in the waitlist \
+					and in the room.");
+				break;
+			case "$log":
+				printChat("The log displays information suchs as woots and mehs for the current song. \
+					You can move the log in the middle of the screen by dragging it \
+					with the top bar. Double click it or enter $reset to move the log back to \
+					it's original position. Use the black button at its bottom right hand corner to hide \
+					or show the log.");
+				break;
+			case "$button":
+				printChat("When a log button is pressed it begins to glow slightly. The blue button \
+					toggles autojoin which makes you automatically join the waitlist if you're not \
+					in it already, while the green button toggles autowoot which makes you woot every song.");
+				break;
+			case "$awaybot":
+				printChat("By clicking the 'Away'-button in the userlist or using the $away command you \
+					automatically reply with a specified message whenever somebody is mentioning you.");
+				break;
+			case "$changes":
+				printChat("Recent changes: Added commands and the waitlist.");
+				break;
+			case "$reset":
+				var log = document.getElementById("log");
+				log.style.top = "288px";
+				log.style.right = "177px";
+				log.style.zIndex = "8";
+				var but1 = document.getElementById("togg");
+				but1.style.top = "280px";
+				but1.style.left = "225px";
+				var but2 = document.getElementById("expwoot");
+				but2.style.top = "255px";
+				but2.style.left = "198px";
+				var but3 = document.getElementById("expjoin");
+				but3.style.top = "230px";
+				but3.style.left = "171px";
+				printChat("Reset the log position.");
+				break;
+			case "$away":
+				if (commandinfo.length > 1)
+					printChat("Working!");
+				document.getElementById("awaybutx").click();
+				break;
+			default:
+				iscommand = false;
+				break;
+		}
+		
+		if (iscommand)
+		{
+			chatin.value = "";
+		}
 	}
 }
 
