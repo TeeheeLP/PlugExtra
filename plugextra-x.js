ff3b9d3860791ff086d16d74da67f0dfddb508c1
TeeheeLP <teeheelp@gmx-topmail.de>
2013-03-15 22:38:48 +0100

    Update plugextra-x.js

--- plugextra-x.js
+++ plugextra-x.js
@@ -821,7 +821,7 @@ function checkOwnIn(e, chatin)
 				{
 					$("#awaybutx").click();
 				}
-				else Models.user.changeStatus(0);
+				else iscommand = true;
 				break;
 			case "$away":
 				if (commandinfo.length > 1 && commandinfo[1] != null && commandinfo[1] != "")
