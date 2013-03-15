f58b597514f021498fea6ca86a30dfdc18dd782e
TeeheeLP <teeheelp@gmx-topmail.de>
2013-03-15 14:37:09 +0100

    Update plugextra.js

--- plugextra.js
+++ plugextra.js
@@ -19,7 +19,7 @@ function dragLog(e)
 	}
 	if (dodrag)
 	{
-		$(document).style.mozUserSelect = "none";
+		$(document).css.mozUserSelect = "none";
 		if (oldcx == "" || oldcx == null)
 		{
 			oldcx = e.pageX;
@@ -82,7 +82,7 @@ function stopDrag()
 {
 	isclicked = false;
 	dodrag = false;
-	$(document).style.mozUserSelect = "all";
+	$(document).css.mozUserSelect = "all";
 	//elem.style.transition = "background 0.5s, opacity 0.5s, height 0.5s";
 	oldcx = ''; 
 	oldcy = ''; 
