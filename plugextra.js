48af02783087a2844e621c0c9241a0732a722187
TeeheeLP <teeheelp@gmx-topmail.de>
2013-03-18 14:56:20 +0100

    Update plugextra.js

--- plugextra.js
+++ plugextra.js
@@ -17,7 +17,7 @@ function dragLog(e)
 	if (isclicked)
 	{
 		if (e.pageX < (parseInt(getComputedStyle(log).marginLeft) + parseInt(log.style.width) 
-			* ($.browser.webkit ? 1 : 1.5) - parseInt(log.style.right) - 20))
+			* ($.browser.webkit ? 1 : 1) - parseInt(log.style.right) - 20))
 		{
 			if (e.pageY < parseInt(log.style.top) + 10) 
 			{
