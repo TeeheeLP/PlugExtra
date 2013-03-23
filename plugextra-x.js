2f1e0206cf788069562d56b6311f087f6e901a55
TeeheeLP <teeheelp@gmx-topmail.de>
2013-03-23 17:41:56 +0100

    Update plugextra-x.js

--- plugextra-x.js
+++ plugextra-x.js
@@ -1211,4 +1211,5 @@ function checkOwnIn(e, chatin)
 
 var chatinput = document.getElementById("chat-input-field");
 chatinput.setAttribute('onkeydown', 'checkOwnIn(event, this);');
-document.getElementById("button-chat-popout").setAttribute("onkeypress", "alert('TEST');");
+document.getElementById("button-chat-popout").setAttribute("onkeyclick", 
+	"setTimeout(function() { Popout.Chat.chatInput.setAttribute('onkeydown', 'alert(event + this.value);'); })");
