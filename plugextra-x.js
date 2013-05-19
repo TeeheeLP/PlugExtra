0939c162f113d80ca0e767d266965bde8cdfc8e3
TeeheeLP <teeheelp@gmx-topmail.de>
2013-05-19 16:23:14 +0300

    Update plugextra-x.js

--- plugextra-x.js
+++ plugextra-x.js
@@ -502,6 +502,8 @@ function loadUser(user, userData, rank)
 	user.id = "pgx" + userData.id;
 	user.style.width = "100%";
 	user.style.marginTop = "5px";
+	user.style.left = "-5px";
+	user.style.paddingLeft = "5px";
 	user.style.borderLeft = "3px solid #444444";
 	if (rank == "admin") user.style.color = "#FF3A97";
 	user.style.cursor = "pointer";
